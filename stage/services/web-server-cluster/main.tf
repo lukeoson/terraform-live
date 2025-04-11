deee603e7d598963ff820521f11b60086dc555cd
Luke Richardson <lukeoson@gmail.com>
2025-04-11 07:50:38 +0100

    added source ref v.0.0.0-alpha to stage/web-server

--- stage/services/web-server-cluster/main.tf
+++ stage/services/web-server-cluster/main.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  #source = "../../../modules/services/webserver-cluster"
+  source = "https://github.com/lukeoson/terraform-modules/tree/main/services/webserver-cluster?ref=v0.0.0-alpha"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "lukeoson-terraform-state-backend"
