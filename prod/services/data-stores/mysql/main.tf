c11ee146bb3f536394b8baa676e953371b04fd22
Luke Richardson <lukeoson@gmail.com>
2025-04-11 07:21:00 +0100

    initial commit

---
+++ prod/services/data-stores/mysql/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    # Your bucket name!
+    bucket         = "lukeoson-terraform-state-backend"
+    key            = "prod/data-stores/mysql/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Your DynamoDB table name!
+    dynamodb_table = "terraform_locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "prod-lukeoson-terraform"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  # How should we set the username and password?
+  username = var.db_username
+  password = var.db_password
+}
